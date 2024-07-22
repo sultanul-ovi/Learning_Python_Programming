54b6ba4147ff114fb182cb142fb3fe698205bf4b
sultanul-ovi <sultanulislamovi@gmail.com>
2024-07-22 08:46:54 -0400

    day 2 done

--- day_01_to_10/day_02_operations.py
+++ day_01_to_10/day_02_operations.py
@@ -4,3 +4,16 @@
 # 6 / 3
 # 6 % 4
 # 6 ** 2
+print(round(8/3))
+print(round(8/3, 2))
+print(8 // 3) #floor value
+
+# result = result/2 is same as result /= 2
+
+# fstring
+
+score = 0
+height = 1.8
+isWinning = True
+print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
+print(6 + 4 / 2 - (1 * 2))
